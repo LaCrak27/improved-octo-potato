c8a0319d146701b6cb6dc995788bde517b3224e8
LaCrak 27 <ivancrak123@gmail.com>
2023-06-28 21:04:44 +0200

    Small fixes

---
+++ Potato_Launcher/bin/Debug/update.bat
@@ -0,0 +1,2 @@
+timeout 3 & xcopy /s /y "C:\Users\ivanc\AppData\Local\TempExample_Launcher.exe" "C:\Users\ivanc\Desktop\Projects\improved-octo-potato\potato_Launcher\bin\Debug\Example_Launcher.exe" && del "C:\Users\ivanc\AppData\Local\TempExample_Launcher.exe"
+start Example_Launcher.exe
